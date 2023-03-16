c2d393ed0998f00301c2760f422d2f8cc07da84e
Mikhail Khalilov <mikhail.khalilov@inf.ethz.ch>
2023-03-16 15:43:36 +0000

    simple batched experiments script

---
+++ configs/edan/simple_kernels/matmul.c
@@ -0,0 +1,51 @@
+/**
+ * A very simple example of the multiplication of
+ * 2 x 2 matrices.
+ * Example:
+ * A = [[1, 2],
+ *      [2, 1]],
+ * B = [[1, 0],
+ *      [0, 1]]
+ * C = A x B = [[1, 2], [2, 1]]
+ */
+#include <stdio.h>
+#define N 256
+
+/**
+ * Main computation kernel that multiplies two square matrices
+ * A and B of dimension n x n, and stores the result in matrix C.
+ */
+int __attribute__ ((noinline)) kernel(int n, int A[n][n], int B[n][n], int C[n][n])
+{
+  for (int i = 0; i < n; ++i)
+  {
+    for (int j = 0; j < n; ++j)
+    {
+      for (int k = 0; k < n; ++k)
+      {
+	C[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
+  return C[0][0];
+}
+
+
+int main(int argc, char **argv)
+{
+  // Array initialization
+  /* int A[N][N] = { {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4} }; */
+  /* int B[N][N] = { {1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1} }; */
+  int i, j;
+  int A[N][N];
+  for (i = 0; i < N; ++i)
+    for (j = 0; j < N; ++j)
+      A[i][j] = i + j;
+  int B[N][N];
+  for (i = 0; i < N; ++i)
+    for (j = 0; j < N; ++j)
+      B[N][N] = 1;
+  int C[N][N] = { 0 };
+
+  return kernel(N, A, B, C);
+}
---
+++ configs/edan/simple_kernels/scaled_vector_add.c
@@ -0,0 +1,43 @@
+/**
+ * An example of addition of two scaled 2D vectors.
+ * Example:
+ * a = 2
+ * b = 3
+ * x = [1, 2]
+ * y = [3, 4]
+ * z = a * x +  b * y = 2 * [1, 2] + 3 * [3, 4] = [11, 16]
+ */
+#define N 1024
+
+#define A 2
+#define B 3
+
+
+/**
+ * Main computation kernel that calculates scaled vector addition
+ * between two vectors a.x and b.y. The result is stored in variable z.
+ */
+int __attribute__ ((noinline)) kernel(int n, int *x, int *y, int *z, int a, int b)
+{
+  for (int i = 0; i < n; ++i)
+    z[i] = a * x[i] + b * y[i];
+
+  return z[0];
+}
+
+
+int main(int argc, char **argv)
+{
+  // Vector initialization
+  int x[N];
+  int y[N];
+  int z[N] = { 0 };
+
+  for (int i = 0; i < N; ++i)
+  {
+    x[i] = i;
+    y[i] = i;
+  }
+
+  return kernel(N, x, y, z, A, B);
+}
